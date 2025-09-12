20c7798873ce50e620a9def5c1a3676fedfa732c
mgocraft <20999763+mgocraft@users.noreply.github.com>
2025-09-12 14:45:54 -0500

    feat: redesign header with tailwind

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,52 +1,12 @@
 // components/NavBar.js
 import Link from 'next/link';
-import styles from './NavBar.module.css';
 
 export default function NavBar() {
-  const links = [
-    { href: '/team/allteamsrecords', label: 'All Teams Records' },
-    { href: '/about', label: 'About' },
-    { href: '/record-book', label: 'Record Book' },
-    {
-      href: '/path-to-conference',
-      label: 'Path To Other Conferences',
-    },
-    { href: '/blog', label: 'Blog' },
-    {
-      href: 'https://amzn.to/46M42Fs',
-      label: 'Buy CFB Belts',
-      external: true,
-    },
-  ];
-
   return (
-    <header className={styles.header}>
-      <nav className={styles.navBar}>
-        <Link href="/" className={styles.title}>
-          College Football Belt
-        </Link>
-
-        <ul className={styles.navList}>
-          {links.map(({ href, label, external }) => (
-            <li key={href}>
-              {external ? (
-                <a
-                  href={href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.navLink}
-                >
-                  {label}
-                </a>
-              ) : (
-                <Link href={href} className={styles.navLink}>
-                  {label}
-                </Link>
-              )}
-            </li>
-          ))}
-        </ul>
-      </nav>
+    <header className="w-full min-h-screen bg-blue-900 flex items-center justify-center text-slate-100">
+      <Link href="/" className="text-5xl font-extrabold tracking-tight">
+        College Football Belt
+      </Link>
     </header>
   );
 }
