2b07f6779cad2fb60261e63beaaddf24ff83c362
mgocraft <20999763+mgocraft@users.noreply.github.com>
2025-11-19 10:19:58 -0600

    Simplify ads.txt route output

--- pages/ads.txt.js
+++ pages/ads.txt.js
@@ -1,11 +1,6 @@
 import { getAdsTxtEntry } from "../utils/adsense";
 
 export default function handler(req, res) {
-  const lines = [
-    "# Authorized digital sellers",
-    getAdsTxtEntry(),
-  ];
-
   res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.status(200).send(`${lines.join("\n")}\n`);
+  res.status(200).send(`${getAdsTxtEntry()}\n`);
 }
